81a99ae54145dc6cd879c889b14d54136b798bd5
QR <1826255833@qq.com>
2022-01-29 12:00:48 +0800

    update fish-node

--- src/views/fishNode/index.vue
+++ src/views/fishNode/index.vue
@@ -1,5 +1,14 @@
 <template>
-  <div class="fish-node-container"></div>
+  <div class="fish-node">
+    <el-descriptions title="鱼缸节点信息">
+      <el-descriptions-item label="节点名称">kooriookami</el-descriptions-item>
+      <el-descriptions-item label="备注">
+        <el-tag size="small">School</el-tag>
+      </el-descriptions-item>
+      <el-descriptions-item label="地址"
+        >No.1188, Wuzhong Avenue, Wuzhong District, Suzhou, Jiangsu
+        Province</el-descriptions-item
+      >
+    </el-descriptions>
+  </div>
 </template>
-
-<script></script>
