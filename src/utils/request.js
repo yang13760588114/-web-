92ce55c9f2994107947a6dba9185d6b5ad5a89da
QR <1826255833@qq.com>
2022-01-30 14:31:25 +0800

    update request

--- src/utils/request.js
+++ src/utils/request.js
@@ -10,6 +10,8 @@ const service = axios.create({
   timeout: 5000, // request timeout
 });
 
+const error = "ERROR!";
+
 // request interceptor
 service.interceptors.request
   .use
@@ -24,60 +26,31 @@ service.interceptors.request
   // }
   ();
 
-// response interceptor
-// service.interceptors.response.use(
-//   /**
-//    * If you want to get http information such as headers or status
-//    * Please return  response => response
-//    */
-
-//   /**
-//    * Determine the request status by custom code
-//    * Here is just an example
-//    * You can also judge the status by HTTP Status Code
-//    */
-//   (response) => {
-//     const res = response.data;
-
-//     // if the custom code is not 20000, it is judged as an error.
-//     if (res.code !== 20000) {
-//       Message({
-//         message: res.message || "Error",
-//         type: "error",
-//         duration: 5 * 1000,
-//       });
-
-//       // 50008: Illegal token; 50012: Other clients logged in; 50014: Token expired;
-//       if (res.code === 50008 || res.code === 50012 || res.code === 50014) {
-//         // to re-login
-//         MessageBox.confirm(
-//           "You have been logged out, you can cancel to stay on this page, or log in again",
-//           "Confirm logout",
-//           {
-//             confirmButtonText: "Re-Login",
-//             cancelButtonText: "Cancel",
-//             type: "warning",
-//           }
-//         ).then(() => {
-//           store.dispatch("user/resetToken").then(() => {
-//             location.reload();
-//           });
-//         });
-//       }
-//       return Promise.reject(new Error(res.message || "Error"));
-//     } else {
-//       return res;
-//     }
-//   },
-//   (error) => {
-//     console.log("err" + error); // for debug
-//     Message({
-//       message: error.message,
-//       type: "error",
-//       duration: 5 * 1000,
-//     });
-//     return Promise.reject(error);
-//   }
-// );
+// 接口响应拦截
+service.interceptors.response.use(
+  (response) => {
+    const res = response.data;
+    // 只有 code == 200 是正确的, 其他都是错误的
+    if (res.code !== 200) {
+      Message({
+        message: res.message || error,
+        type: "error",
+        duration: 1000,
+      });
+      return Promise.reject(new Error(res.message || error));
+    } else {
+      return res;
+    }
+  },
+  (error) => {
+    console.log("err" + error); // for debug
+    Message({
+      message: error.message,
+      type: "error",
+      duration: 5 * 1000,
+    });
+    return Promise.reject(error);
+  }
+);
 
 export default service;
