6dcca894884de119e0640de156c71c53b67b1bb2
QR <1826255833@qq.com>
2022-02-14 15:16:54 +0800

    socket update

--- src/views/record/realTime/index.vue
+++ src/views/record/realTime/index.vue
@@ -10,40 +10,30 @@ export default {
     // 页面创建生命周期函数
     this.initWebSocket();
   },
-  destroyed: function () {
+  destroyed() {
     // 离开页面生命周期函数
     this.websocketclose();
   },
   methods: {
-    collapse: function () {
-      this.isCollapse = !this.isCollapse;
-      if (this.isCollapse) {
-        this.iconClass = "cebianlanzhankai";
-      } else {
-        this.iconClass = "cebianlanshouhui";
-      }
-    },
-    initWebSocket: function () {
+    initWebSocket() {
       // WebSocket与普通的请求所用协议有所不同，ws等同于http，wss等同于https
-      this.websock = new WebSocket("ws://localhost/websocket/DPS007");
+      this.websock = new WebSocket("ws://localhost/znyg/socket/qr");
       this.websock.onopen = this.websocketonopen;
       this.websock.onerror = this.websocketonerror;
       this.websock.onmessage = this.websocketonmessage;
       this.websock.onclose = this.websocketclose;
     },
-    websocketonopen: function () {
+    websocketonopen() {
       console.log("WebSocket连接成功");
     },
-    websocketonerror: function (e) {
+    websocketonerror() {
       console.log("WebSocket连接发生错误");
     },
-    websocketonmessage: function (e) {
-      var da = JSON.parse(e.data);
-      console.log(da);
-      this.message = da;
+    websocketonmessage(e) {
+      console.log(`Get Message: ${e.data}`);
     },
-    websocketclose: function (e) {
-      console.log("connection closed (" + e.code + ")");
+    websocketclose() {
+      console.log(`connection closed`);
     },
   },
 };
