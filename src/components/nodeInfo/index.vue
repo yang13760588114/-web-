6df0827cecf36dcdb63c99e8255bd463bffe11b9
QR <1826255833@qq.com>
2022-02-24 09:57:48 +0800

    update

--- src/components/nodeInfo/index.vue
+++ src/components/nodeInfo/index.vue
@@ -5,8 +5,11 @@
       <el-container>
         <el-aside width="200px">Aside</el-aside>
         <el-main>
+          <v-chart class="chart" :option="option"></v-chart>
           <div>temperature: {{ temperatures }}</div>
           <div>dates: {{ dates }}</div>
+          <!-- 为 ECharts 准备一个定义了宽高的 DOM -->
+          <div id="main" style="width: 600px; height: 400px"></div>
         </el-main>
       </el-container>
     </el-container>
@@ -15,6 +18,34 @@
 
 <script>
 import { realTimeRecords } from "@/api/record";
+
+// 基于准备好的dom，初始化echarts实例
+var myChart = echarts.init(document.getElementById("main"));
+
+// 指定图表的配置项和数据
+var option = {
+  title: {
+    text: "ECharts 入门示例",
+  },
+  tooltip: {},
+  legend: {
+    data: ["销量"],
+  },
+  xAxis: {
+    data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"],
+  },
+  yAxis: {},
+  series: [
+    {
+      name: "销量",
+      type: "bar",
+      data: [5, 20, 36, 10, 10, 20],
+    },
+  ],
+};
+
+// 使用刚指定的配置项和数据显示图表。
+myChart.setOption(option);
 
 export default {
   name: "nodeInfo",
