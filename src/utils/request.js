a3e7b3de225847384e898757bf48b55b7b8e77b6
QR <1826255833@qq.com>
2022-02-25 13:49:41 +0800

    update request.js

--- src/utils/request.js
+++ src/utils/request.js
@@ -3,12 +3,12 @@ import { Message } from "element-ui";
 import store from "@/store";
 import { getToken } from "@/utils/auth";
 
-axios.defaults.withCredentials = true; //Cookie跨域
+// axios.defaults.withCredentials = true; //Cookie跨域
 
 // create an axios instance
 const service = axios.create({
   baseURL: process.env.VUE_APP_BASE_API, // url = base url + request url
-  // withCredentials: true, // send cookies when cross-domain requests
+  withCredentials: true, // send cookies when cross-domain requests
   timeout: 5000, // request timeout
 });
 
