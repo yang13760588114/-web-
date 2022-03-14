71e614d5ed3e32dc26a036c35c928ac8c69680fc
QR <1826255833@qq.com>
2022-03-14 13:49:20 +0800

    用户表信息修改

--- src/views/user/index.vue
+++ src/views/user/index.vue
@@ -1,6 +1,18 @@
 <template>
   <div class="dashboard-container">
-    <userCard :userInfo="userInfo"></userCard>
+    <div class="left">
+      <userCard :userInfo="userInfo" class="card"></userCard>
+      <div class="clock">{{ hour }}:{{ minute }}:{{ second }}</div>
+    </div>
+    <el-calendar class="calendar" v-model="date">
+      <!-- 这里使用的是 2.5 slot 语法，对于新项目请使用 2.6 slot 语法-->
+      <template slot="dateCell" slot-scope="{ data }">
+        <p :class="data.isSelected ? 'is-selected' : ''">
+          {{ data.day.split("-").slice(1).join("-") }}
+          {{ data.isSelected ? "" : "" }}
+        </p>
+      </template>
+    </el-calendar>
   </div>
 </template>
 
@@ -13,6 +25,10 @@ export default {
   data() {
     return {
       userInfo: null,
+      date: new Date(),
+      hour: null,
+      minute: null,
+      second: null,
     };
   },
   methods: {
@@ -21,14 +37,41 @@ export default {
       // 这里需要用 JSON 转换一下, 应为 cookie 存的东西再取出来是字符串
       this.userInfo = JSON.parse(user);
     },
+    fillZero(num) {
+      return num < 10 ? "0" + num : num;
+    },
   },
   created() {
     this.mountUserInfo();
+    setInterval(() => {
+      let date = new Date();
+      this.second = this.fillZero(date.getSeconds());
+      this.minute = this.fillZero(date.getMinutes());
+      this.hour = this.fillZero(date.getHours());
+    }, 1000);
   },
 };
 </script>
 
 <style lang="scss" scoped>
+.dashboard-container {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+}
+.clock {
+  font-size: 30px;
+  font-weight: bold;
+  margin-top: 50px;
+  margin-left: 70px;
+}
+.calendar {
+  height: 50%;
+  width: 70%;
+}
+.card {
+  height: 60%;
+}
 .dashboard {
   &-container {
     margin: 30px;
@@ -37,5 +80,8 @@ export default {
     font-size: 30px;
     line-height: 46px;
   }
+}
+.is-selected {
+  color: #1989fa;
 }
 </style>
