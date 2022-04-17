67871195f29dffa567b2e1bb9bbe3945cf681bb1
QR <1826255833@qq.com>
2022-04-17 12:35:25 +0800

    侧边栏修改

--- src/layout/components/Navbar.vue
+++ src/layout/components/Navbar.vue
@@ -16,7 +16,7 @@
         </div>
         <el-dropdown-menu slot="dropdown" class="user-dropdown">
           <router-link to="/">
-            <el-dropdown-item> 首页 </el-dropdown-item>
+            <el-dropdown-item> 鱼缸监测和控制 </el-dropdown-item>
           </router-link>
           <el-dropdown-item divided @click.native="logout">
             <span style="display: block">退出</span>
